39fa74b5436e382296455872ee4415ff98209710
Hamzah Yahya <wokabi108@gmail.com>
2025-04-19 12:55:40 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project.supabase.co'; // Ganti dengan URL Supabase-mu
-const supabaseKey = 'your-anon-key'; // Ganti dengan anon key Supabase-mu
+const supabaseUrl = 'https://lbduiafnkdbdiralxfaa.supabase.co'; // Ganti dengan URL Supabase-mu
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiZHVpYWZua2RiZGlyYWx4ZmFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNDE0MjcsImV4cCI6MjA2MDYxNzQyN30.5Z5aHwVDXJJsNjuAA0ETdVvL3EzbMI-nH3NVgnuKyFU'; // Ganti dengan anon key Supabase-mu
 export const supabase = createClient(supabaseUrl, supabaseKey);
