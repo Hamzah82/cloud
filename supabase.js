4d0062e988212a3748f149b0934e27759a15dd89
Hamzah Yahya <wokabi108@gmail.com>
2025-04-19 13:26:50 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
-// supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = 'https://lbduiafnkdbdiralxfaa.supabase.co'; // Ganti dengan URL Supabase-mu
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiZHVpYWZua2RiZGlyYWx4ZmFhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTA0MTQyNywiZXhwIjoyMDYwNjE3NDI3fQ.4avtfBPYF2xQzoPNU6wKceceM2ssYPW1SwUThbzV1PE'; // Ganti dengan anon key Supabase-mu
+const supabaseUrl = "https://lbduiafnkdbdiralxfaa.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiZHVpYWZua2RiZGlyYWx4ZmFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNDE0MjcsImV4cCI6MjA2MDYxNzQyN30.5Z5aHwVDXJJsNjuAA0ETdVvL3EzbMI-nH3NVgnuKyFU";
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
